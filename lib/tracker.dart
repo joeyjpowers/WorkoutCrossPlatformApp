faf017b578fbf12209b84c27144475d22b3926ff
joeypowers <jjpowers@purdue.edu>
2022-04-28 21:13:51 -0400

    primary front end for first page of set tracker

--- lib/tracker.dart
+++ lib/tracker.dart
@@ -1,23 +1,65 @@
 import 'package:flutter/material.dart';
 
-class Tracker extends StatelessWidget {
-
+class Tracker extends StatefulWidget {
   final VoidCallback setHome;
   Tracker(this.setHome);
 
   @override
+  State<Tracker> createState() => _TrackerState();
+}
+class _TrackerState extends State<Tracker> {
+  double _numWorkouts = 3;
+
+  @override
   Widget build(BuildContext context) {
+
+
     return Container(
-      child: Center(
-        child: RaisedButton(
-          child: Text("Tracker Home",
-            style: TextStyle(color: Colors.white, fontSize: 48),
+      child: Column(children: [
+        Container(
+          child: Center(
+            child: Text(
+              "How many workouts today?",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 36,
+                color: Colors.white,
+              ),
+            ),
           ),
-          color: Colors.red[900],
-          onPressed: setHome,
+          margin: EdgeInsets.only(left: 20, top: 100, right: 20),
         ),
-      ),
+        SizedBox(height: 20,),
+        Slider(
+          value: _numWorkouts,
+          max: 6,
+          divisions: 6,
+          label: _numWorkouts.round().toString(),
+          onChanged: (double value) {
+            setState(() {
+              _numWorkouts = value;
+            });
+          },
+        ),
+        Spacer(),
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(bottom: 8, top: 20),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: RaisedButton(
+                child: Text(
+                  "Tracker Home",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+                color: Colors.red[900],
+                onPressed: widget.setHome,
+              ),
+            ),
+          ),
+        ),
+      ]),
       color: Colors.black54,
-    );;
+    );
   }
 }
